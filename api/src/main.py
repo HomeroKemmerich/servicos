c63770af256754b0e8a64f57d297a7b03b143571
Homero Kemmerich <146676@upf.br>
2023-10-19 22:00:27 -0300

    Estrutura do CRUD

--- api/src/main.py
+++ api/src/main.py
@@ -1,7 +1,30 @@
 from fastapi import FastAPI
+from .models.machine import Machine
+
+
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "API de serviços de máquinas"}
+
+@app.get("/items/")
+async def read_items():
+    return []
+
+@app.get("/items/{machine_id}")
+async def read_item(machine_id: int):
+    return {"item_id": machine_id}
+
+@app.post("/items/")
+async def create_item(machine: Machine):
+    return machine
+
+@app.put("/items/{machine_id}")
+async def update_item(machine_id: int, machine: Machine):
+    return {"item_name": machine.name, "item_id": machine_id}
+
+@app.delete("/items/{machine_id}")
+async def delete_item(machine_id: int):
+    return {"item_id": machine_id}
